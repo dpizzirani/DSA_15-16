b13d3abd7aa9f05524e5e3b24cdd0feddfb6bab7
dpizzirani <davide@pietropizzirani.com>
2016-09-12 12:08:19 +0200

    Finished Lab1's ArrayUtility.java file.

--- src/ArrayUtility.java
+++ src/ArrayUtility.java
@@ -125,22 +125,105 @@ public class ArrayUtility {
     public static int[] copyArray(int[] A) {
         int B[] = new int[A.length];
 
+        for (int i=0; i<B.length; i++) {
+            B[i] = A[i];
+        }
 
         return B;
     }
 
 
+    public static int[][] copyMatrix(int[][] A) {
+        int B[][] = new int[A.length][A[0].length];
+
+        for (int r=0; r<B.length; r++) {
+            for (int c=0; c<B[r].length; c++) {
+                B[r][c] = A[r][c];
+            }
+        }
+
+        return B;
+    }
+
+
+    public static int findInArray(int[] A, int q) {
+        for (int i=0; i<A.length; i++) {
+            if (A[i] == q)
+                return i;
+        }
+
+        return -1;
+    }
+
+
+    public static int findInSortedArray(int[] A, int q) {
+        return binarySearchRecursive(A, q, 0, A.length-1);
+    }
+
+
+    public static int binarySearchRecursive(int[] A, int q, int l, int r) {
+        if (l > r)
+            return -1;
+
+        int m = (l + r)/2;
+
+        if (A[m] == q)
+            return m;
+        else if (A[m] < q)
+            return binarySearchRecursive(A, q, m+1, r);
+        else
+            return binarySearchRecursive(A, q, l, m);
+    }
+
+
+    public static int findFirstInSortedArrary(int[] A, int q) {
+        return findFirstInSortedArraryAux(A, q, 0, A.length-1);
+    }
+
+
+    /*public static int findFirstInSortedArraryAux(int[] A, int q, int l, int r) {
+        if (l == 0 && A[q] == q) {
+            if (A[l] == q)
+                return l;
+            else
+                return -1;
+        }
+
+        int m = (int)Math.floor((l+r)/2);
+
+        if (A[m] < q)
+            return findFirstInSortedArraryAux(A, q, m+1, r);
+        else
+            return findFirstInSortedArraryAux(A, q, l, m);
+    }*/
+
+
+    public static int findFirstInSortedArraryAux(int[] A, int q, int l, int r) {
+        if (l > r)
+            return -1;
+
+        int m = (int)Math.floor((l + r)/2);
+
+        if (A[l] == q)
+            return l;
+        else if (A[m] < q)
+            return binarySearchRecursive(A, q, m+1, r);
+        else
+            return binarySearchRecursive(A, q, l, m);
+    }
+
+
     public static void main(String[] args) {
-        //int[] A = {5, 4, 10, 9, 8, 4};
-        /*int[] A = createRandomArray(7, 0, 25);
+        //int[] A = {1, 3, 4, 6, 7, 9, 11, 13, 24, 45, 56, 67, 78, 78, 123, 145};
+        //int[] A = createRandomArray(7, 0, 25);
+        int[] A = {1, 1, 2, 3, 3, 5, 5, 7, 7, 7, 9};
 
         System.out.println(Arrays.toString(A));
-        shiftLeft(A, 1, 5);
-        System.out.println(Arrays.toString(A));*/
+        //System.out.println(findInSortedArray(A, 7));
+        //System.out.println(findInArray(A, 7));
+        //System.out.println(findFirstInSortedArrary(A, 5));
 
-        int[][] M = createRandomMatrix(2, 10, 0, 25);
-
-        printMatrix(M);
+        System.out.println(Arrays.toString(A));
 
 
     }
